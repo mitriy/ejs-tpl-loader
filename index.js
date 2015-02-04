271f8a4094e6789bfd3e436cac22b33e552fafdf
Jan Nicklas <j.nicklas@me.com>
2015-02-04 15:02:35 +0100

    Add github pages

--- index.js
+++ index.js
@@ -1,41 +1,18 @@
-var _ = require('lodash');
-var loaderUtils = require('loader-utils');
-var htmlMinifier = require('html-minifier');
-
-function parseBoolean(s) {
-  return (s === 'true' || s === 'false') ? s === 'true' : s;
+var loader = require('html-tpl-loader');
+window._ = require('lodash');
+window.compile = function(){
+	var js = document.getElementById('input-js').value;
+	var tpl = document.getElementById('input-tpl').value;
+	// Webpack Require Shim
+	var require = function(url){
+		if(url.indexOf('html-tpl') !== 0) {
+			return;
+		}
+		var query = url.indexOf('?') > 0 ? url.replace(/^.+\?/, '?') : '';
+		var module = {};
+		eval(loader.call({query: query}, tpl));
+		return module.exports;
+	};
+	// Execute js
+	eval(js);
 }
-
-module.exports = function (source) {
-  this.cacheable && this.cacheable();
-  var query = loaderUtils.parseQuery(this.query);
-  var minimize = parseBoolean(query.minimize);
-  if(typeof minimize === "boolean" ? minimize : this.minimize) {
-    source = htmlMinifier.minify(source, {
-      removeComments: !!parseBoolean(query.removeComments),
-      collapseWhitespace: !!parseBoolean(query.collapseWhitespace),
-      collapseBooleanAttributes: !!parseBoolean(query.collapseBooleanAttributes),
-      removeAttributeQuotes: !!parseBoolean(query.removeAttributeQuotes),
-      removeRedundantAttributes: !!parseBoolean(query.removeRedundantAttributes),
-      useShortDoctype: !!parseBoolean(query.useShortDoctype),
-      removeEmptyAttributes: !!parseBoolean(query.removeEmptyAttributes),
-      removeOptionalTags: !!parseBoolean(query.removeOptionalTags)
-    });
-  }
-
-  var tplSettings = {};
-  ['escape', 'interpolate', 'evaluate'].forEach(function(tplSettingName) {
-    if(typeof query[tplSettingName] === "string") {
-      tplSettings[tplSettingName] = new RegExp(query[tplSettingName], 'gm');
-    }
-  });
-
-  var template = _.template(source, null, tplSettings);
-  var jsSource = 'module.exports = ' + template;
-
-  if (parseBoolean(query.lodash)) {
-    jsSource = 'var _ = require("lodash");' + jsSource;
-  }
-
-  return jsSource;
-};
