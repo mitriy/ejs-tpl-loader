bc07afbf41618844a9062bad62e082f8360120f4
Jan Nicklas <j.nicklas@me.com>
2016-01-19 10:21:24 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -2,24 +2,20 @@ var _ = require('lodash');
 var loaderUtils = require('loader-utils');
 var htmlMinifier = require('html-minifier');
 
-function parseBoolean(s) {
-  return (s === 'true' || s === 'false') ? s === 'true' : s;
-}
-
 module.exports = function (source) {
   this.cacheable && this.cacheable();
   var query = loaderUtils.parseQuery(this.query);
-  var minimize = parseBoolean(query.minimize);
+  var minimize = query.minimize;
   if(typeof minimize === "boolean" ? minimize : this.minimize) {
     source = htmlMinifier.minify(source, {
-      removeComments: !!parseBoolean(query.removeComments),
-      collapseWhitespace: !!parseBoolean(query.collapseWhitespace),
-      collapseBooleanAttributes: !!parseBoolean(query.collapseBooleanAttributes),
-      removeAttributeQuotes: !!parseBoolean(query.removeAttributeQuotes),
-      removeRedundantAttributes: !!parseBoolean(query.removeRedundantAttributes),
-      useShortDoctype: !!parseBoolean(query.useShortDoctype),
-      removeEmptyAttributes: !!parseBoolean(query.removeEmptyAttributes),
-      removeOptionalTags: !!parseBoolean(query.removeOptionalTags)
+      removeComments: query.removeComments !== false,
+      collapseWhitespace: !!query.collapseWhitespace,
+      collapseBooleanAttributes: query.collapseBooleanAttributes !== false,
+      removeAttributeQuotes: query.removeAttributeQuotes !== false,
+      removeRedundantAttributes: query.removeRedundantAttributes !== false,
+      useShortDoctype: query.useShortDoctype !== false,
+      removeEmptyAttributes: query.removeEmptyAttributes !== false,
+      removeOptionalTags: !!query.removeOptionalTags
     });
   }
 
@@ -30,10 +26,10 @@ module.exports = function (source) {
     }
   });
 
-  var template = _.template(source, null, tplSettings);
+  var template = _.template(source, tplSettings);
   var jsSource = 'module.exports = ' + template;
 
-  if (parseBoolean(query.lodash)) {
+  if (query.lodash) {
     jsSource = 'var _ = require("lodash");' + jsSource;
   }
 
